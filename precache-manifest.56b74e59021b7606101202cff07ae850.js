b17b98902abf5bb74f3146d497fd8e41b2752286
mariemadeleine <mariemadeleine@mariemadeleines-MacBook-Air.local>
2022-01-03 19:52:07 +0800

    Updates

---
+++ precache-manifest.56b74e59021b7606101202cff07ae850.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fffeb2d6edfaa92d81ccf8d29214e83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fe2bc90e74bff5c62ad",
+    "url": "/static/css/2.3a0ac4c9.chunk.css"
+  },
+  {
+    "revision": "8c7233ffab7c30677da5",
+    "url": "/static/css/main.82576c36.chunk.css"
+  },
+  {
+    "revision": "6fe2bc90e74bff5c62ad",
+    "url": "/static/js/2.0a46497c.chunk.js"
+  },
+  {
+    "revision": "958608aaef3e81c9b20a22457416f406",
+    "url": "/static/js/2.0a46497c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7233ffab7c30677da5",
+    "url": "/static/js/main.dff225e0.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "7687d7270180dfe87a64ad3841920803",
+    "url": "/static/media/logo.7687d727.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7938f370f6484c0ab1fbe8a9cc058a4e.js"
+  "/precache-manifest.56b74e59021b7606101202cff07ae850.js"
 );
 
 self.addEventListener('message', (event) => {
